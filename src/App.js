277c262333244512cf928d8b3cc3db07f0992368
Ayush Singh <ayushsingh28082001@gmail.com>
2024-09-03 14:07:30 +0530

    Your descriptive commit message

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <h1 className='text-red-400 font-bold text-3xl '>Jai Shree Ram</h1>
+      <h1 className='text-red-400 font-bold text-3xl '>Jai Shree Krishna</h1>
     </div>
   );
 }
