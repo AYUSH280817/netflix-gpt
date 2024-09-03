3f8fe695e56f392914398e9fba21ba3c578d2a52
Ayush Singh <ayushsingh28082001@gmail.com>
2024-09-03 14:04:18 +0530

    Add all project files

---
+++ src/App.js
@@ -0,0 +1,11 @@
+
+import './App.css';
+function App() {
+  return (
+    <div className="App">
+      <h1 className='text-red-400 font-bold text-3xl '>Jai Shree Ram</h1>
+    </div>
+  );
+}
+
+export default App;
